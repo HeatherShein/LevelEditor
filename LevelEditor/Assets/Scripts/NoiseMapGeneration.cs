ce05969e034814eefffb98ca9589c0ebef0aa104
HeatherShein <colin.davidson@hotmail.fr>
2020-03-03 16:31:24 +0000

    Adding noise generation script + level tile
    Noise generation will be used to generate pseudo random noise.
    
    Tile Generation will assign different texture to a tile depending on the noise at this point.
    
    Here the generation will modify the height, but it can be used for other parameters.
    
    Added a ground plane to see the changes.

---
+++ LevelEditor/Assets/Scripts/NoiseMapGeneration.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseMapGeneration : MonoBehaviour
+{
+
+    public float[,] GenerateNoiseMap(int mapDepth, int mapWidth, float scale)
+    {
+        // Create an empty noise map with the mapDepth and mapWidth coordinates
+        float[,] noiseMap = new float[mapDepth, mapWidth];
+
+        for (int zIndex = 0; zIndex < mapDepth; zIndex++)
+        {
+            for (int xIndex = 0; xIndex < mapWidth; xIndex++)
+            {
+                // Calculate sample indices based on the coordinates and the scale
+                float sampleX = xIndex / scale;
+                float sampleZ = zIndex / scale;
+
+                // Generate noise value using PerlinNoise
+                float noise = Mathf.PerlinNoise(sampleX, sampleZ);
+
+                noiseMap[zIndex, xIndex] = noise;
+            }
+        }
+
+        return noiseMap;
+    }
+}
---
+++ LevelEditor/Assets/Scripts/TileGeneration.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGeneration : MonoBehaviour
+{
+
+    [SerializeField]
+    NoiseMapGeneration noiseMapGeneration;
+
+    [SerializeField]
+    private MeshRenderer tileRenderer;
+
+    [SerializeField]
+    private MeshFilter meshFilter;
+
+    [SerializeField]
+    private MeshCollider meshCollider;
+
+    [SerializeField]
+    private float mapScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateTile();
+    }
+
+    void GenerateTile()
+    {
+        // Calculate tile depth and width based on the mesh vertices
+        Vector3[] meshVertices = this.meshFilter.mesh.vertices;
+        int tileDepth = (int)Mathf.Sqrt(meshVertices.Length);
+        int tileWidth = tileDepth;
+
+        // Calculate the offset based on the tile position
+        float[,] heightMap = this.noiseMapGeneration.GenerateNoiseMap(tileDepth,tileWidth,this.mapScale);
+
+        // Generate a height map using noise
+        Texture2D tileTexture = BuildTexture(heightMap);
+
+        this.tileRenderer.material.mainTexture = tileTexture;
+    }
+
+    private Texture2D BuildTexture(float[,] heightMap)
+    {
+        int tileDepth = heightMap.GetLength(0);
+        int tileWidth = heightMap.GetLength(1);
+
+        Color[] colorMap = new Color[tileDepth * tileWidth];
+        
+        for (int zIndex = 0; zIndex < tileDepth; zIndex++)
+        {
+            for (int xIndex = 0; xIndex < tileWidth; xIndex++)
+            {
+                // Transform the 2D Map index in an Array index
+                int colorIndex = zIndex * tileWidth + xIndex;
+                float height = heightMap[zIndex, xIndex];
+                // Assign as color a shade of grey proportional to the height value
+                colorMap[colorIndex] = Color.Lerp(Color.black, Color.white, height);
+            }
+        }
+        // Create a new texture and set its pixel colors
+        Texture2D tileTexture = new Texture2D(tileWidth, tileDepth);
+        tileTexture.wrapMode = TextureWrapMode.Clamp;
+        tileTexture.SetPixels(colorMap);
+        tileTexture.Apply();
+
+        return tileTexture;
+    }
+}
