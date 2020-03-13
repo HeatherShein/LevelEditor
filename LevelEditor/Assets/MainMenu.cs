6616f59bbe97c55b9cbdd74f58fc0ada65ffd74c
AlexisBZH <60657690+AlexisBZH@users.noreply.github.com>
2020-03-13 20:47:51 +0000

    MainMenu

---
+++ LevelEditor/Assets/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void CreatMap() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitMenu(){
+        Debug.Log("QUIT!!");
+        Application.Quit();
+    }
+}
