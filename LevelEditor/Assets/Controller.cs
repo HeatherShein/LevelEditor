f8f1d7214164baee4e38b713814da792786652f1
AlexisBZH <60657690+AlexisBZH@users.noreply.github.com>
2020-03-15 18:40:23 +0000

    CreateMenu

---
+++ LevelEditor/Assets/Controller.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public void GetInput(string guess){
+        Debug.Log("You Entered " + guess);
+    }
+}
