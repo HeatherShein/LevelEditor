e6f55e3d7366c51adad791bb1020befa14c4640d
HeatherShein <colin.davidson@hotmail.fr>
2020-03-26 19:38:10 +0000

    Setup level spawning

---
+++ LevelEditor/Assets/Scripts/MainMenu/PositionCorrection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PositionCorrection : MonoBehaviour
+{
+    [SerializeField]
+    GameObject level;
+
+    [SerializeField]
+    float offsetX;
+
+    [SerializeField]
+    float offsetY;
+
+    [SerializeField]
+    float offsetZ;
+
+    public void CorrectPosition()
+    {
+        Vector3 pos = new Vector3(offsetX, offsetY,offsetZ);
+        this.level.transform.position = this.level.transform.position + pos;
+    }
+}
